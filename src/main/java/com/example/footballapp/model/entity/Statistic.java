77a80d9108c47befbe472d6bb991861432cedadb
quanm200699 <maplestory2012vn@gmail.com>
2021-06-18 21:02:38 +0700

    add statistic entity

---
+++ src/main/java/com/example/footballapp/model/entity/Statistic.java
@@ -0,0 +1,27 @@
+package com.example.footballapp.model.entity;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class Statistic {
+    @Id
+    private Long id;
+
+    private String shotOnGoal;
+
+    private String totalShot;
+
+    private String cornerKick;
+
+    private String goalKeeperSave;
+
+    private String yellowCard;
+
+    private String foul;
+
+    private String offside;
+}
