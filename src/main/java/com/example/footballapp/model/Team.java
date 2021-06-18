df805f03ddba921b4f56977c9fb1260c30a6bef8
quanm200699 <maplestory2012vn@gmail.com>
2021-06-18 20:40:32 +0700

    add team entity

---
+++ src/main/java/com/example/footballapp/model/Team.java
@@ -0,0 +1,19 @@
+package com.example.footballapp.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class Team {
+    @Id
+    private Long id;
+
+    private String name;
+
+    private String code;
+
+    private String logo;
+}
