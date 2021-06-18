63c7304ec3600ba52c018543fd43180097430fc2
quanm200699 <maplestory2012vn@gmail.com>
2021-06-18 22:00:33 +0700

    add statistic service

---
+++ src/main/java/com/example/footballapp/service/statistic/IStatisticService.java
@@ -0,0 +1,7 @@
+package com.example.footballapp.service.statistic;
+
+import com.example.footballapp.entity.Statistic;
+import com.example.footballapp.service.IGeneralService;
+
+public interface IStatisticService extends IGeneralService<Statistic> {
+}
