bc09b5735bf01177b8cd338432368a70cb162c9f
quanm200699 <maplestory2012vn@gmail.com>
2021-06-18 20:21:54 +0700

    add help.md

---
+++ src/test/java/com/example/footballapp/FootballAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.footballapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
